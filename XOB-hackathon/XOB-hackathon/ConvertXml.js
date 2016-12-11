a66e2b12258167f3aa0b13d4abbc108f3c2fe403
Pratik <pratik.kumarpsc@gmail.com>
2016-12-11 17:10:47 +0530

    task

---
+++ XOB-hackathon/XOB-hackathon/ConvertXml.js
@@ -0,0 +1,20 @@
+﻿function loadXMLDoc(dname) {
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
+    }
+    else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.open("GET", dname, false);
+    xhttp.send();
+    return xhttp.responseXML;
+}
+var xmlDoc = loadXMLDoc("Posts.xml");
+var x2js = new X2JS();
+var jsonObj = x2js.xml2json(xmlDoc);
+
+
+console.log(jsonObj.posts.row[0]);
+console.log(jsonObj.posts.row[1]);
+console.log(jsonObj.posts.row[2]);
+console.log(jsonObj.posts.row[3]);
---
+++ XOB-hackathon/XOB-hackathon/myApp.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module('xob', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+app.filter('postId1Only', function () {
+    return function (input) {
+        var postOut = [];
+        angular.forEach(input, function (postType) {
+            if (postType._PostTypeId == '1') {
+                postOut.push(postType);
+            }
+        });
+        return postOut;
+    };
+});
+app.controller('posts', function ($scope,$filter) {
+    $scope.postData = jsonObj;
+    $scope.postData.posts.row = $filter('postId1Only')($scope.postData.posts.row);
+    $scope.totalPostCount = $scope.postData.posts.row.length;
+    $scope.currentPostPage = 1;
+
+    $scope.setvoteup = function (post) {
+        localStorage.setItem(post.votes, post.votes + 1);
+    };
+    //$scope.pageChanged = function () {
+    //    $scope.currentPostPage = $scope.currentPostPage+1;
+    //};
+ 
+});
